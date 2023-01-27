ae3d84b18e76d6a1e35b7020152f0ccbab78d544
Yevhen Liaskivskyi <benatanlev@icloud.com>
2023-01-27 18:28:30 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const body = document.querySelector('body');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+startBtn.style.cursor = 'pointer';
+stopBtn.style.cursor = 'pointer';
+
+stopBtn.setAttribute('disabled', '');
+let timerId;
+startBtn.addEventListener('click', () => {
+  changeBodyColor();
+  timerId = setInterval(changeBodyColor, 1000);
+});
+stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  stopBtn.setAttribute('disabled', '');
+  startBtn.removeAttribute('disabled');
+});
+
+function changeBodyColor() {
+  body.style.backgroundColor = getRandomHexColor();
+  startBtn.setAttribute('disabled', '');
+  stopBtn.removeAttribute('disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
